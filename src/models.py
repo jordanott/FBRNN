9735ecc6f66aa472df891731c1ba7ef73431193c
Jordan Ott <jordan.ott@ABLEKNOWWEAK.workdayinternal.com>
2018-07-30 18:51:34 -0700

    Encoder decoder

--- src/models.py
+++ src/models.py
@@ -51,7 +51,6 @@ class RNN(nn.Module):
         return out_vecs
 
 class FBRNN(nn.Module):
-
     def __init__(self, hidden_size, num_layers, batch_size, feature_size, attention_hidden_size,embedding_dim=None, vocab_size=None):
         super(FBRNN, self).__init__()
         # HyperParameters
@@ -79,7 +78,7 @@ class FBRNN(nn.Module):
     def forward(self, batch):
         #embedded_input = self.embedding(batch)
         batch_size = batch.shape[1]
-        
+
         h_s = [torch.zeros((self.batch_size, self.hidden_size)) for i in range(self.num_layers)]
 
         out_vecs = []
@@ -106,50 +105,103 @@ class FBRNN(nn.Module):
             out_vecs.append(fc2)
         return out_vecs
 
-'''
-with embeddings
-class FBRNN(nn.Module):
 
-    def __init__(self, embedding_dim, vocab_size, hidden_size, num_layers, attention_hidden_size):
-        super(FBRNN, self).__init__()
+class FBRNN_encoder(nn.Module):
+    def __init__(self, hidden_size, num_layers, attention_hidden_size,embedding_dim=None, vocab_size=None):
+        super(FBRNN_encoder, self).__init__()
         # HyperParameters
         self.embedding_dim = embedding_dim
-        self.vocab_size = vocab_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.attention_hidden_size = attention_hidden_size
 
         # Parameters
-        self.embedding = nn.Embedding(embedding_dim=embedding_dim, num_embeddings=vocab_size)
+        self.embedding = nn.Embedding(num_embeddings=vocab_size,embedding_dim=embedding_dim)
         self.cells = nn.ModuleList([nn.GRUCell(input_size=embedding_dim, hidden_size=hidden_size)] +
                                    [nn.GRUCell(input_size=hidden_size, hidden_size=hidden_size) for i in range(num_layers-1)])
         self.hidden_attentions = nn.ModuleList([SelfAttention(input_vector_size=hidden_size, hidden_size=attention_hidden_size) for i in range(num_layers)])
-        self.fc = nn.Linear(16,1)
+
+    def init_hidden(self):
+        self.h_s = [torch.zeros((1,self.hidden_size)) for i in range(self.num_layers)]
 
     def forward(self, batch):
         embedded_input = self.embedding(batch)
         batch_size = batch.shape[1]
-        h_s = [torch.randn((batch_size, self.hidden_size)) for i in range(self.num_layers)]
 
         out_vecs = []
         for token in embedded_input.split(1):
             input_vec = token.squeeze().view(1,self.embedding_dim)
             # get all outputs (go up)
             new_h_s = []
-            for h, cell in zip(h_s, self.cells):
-                print('input')
-                print(h.shape)
-                print(input_vec.shape)
+            for h, cell in zip(self.h_s, self.cells):
                 input_vec = cell(input_vec, h)
-                print(input_vec.shape)
                 new_h_s.append(input_vec)
 
-            h_s = []
+            self.h_s = []
             # compute new hidden states using attention (go right)
             for i, att in enumerate(self.hidden_attentions):
-                h_s.append(att.combine(torch.stack(new_h_s[i:])))
+                self.h_s.append(att.combine(torch.stack(new_h_s[i:])))
 
-            output = self.fc(h_s[-1].squeeze())
-            out_vecs.append(output.data)
+            out_vecs.append(self.h_s[-1].squeeze())
         return out_vecs
-'''
+
+
+class FBRNN_decoder(nn.Module):
+    def __init__(self, hidden_size, num_layers, attention_hidden_size,embedding_dim=None, vocab_size=None):
+        super(FBRNN_decoder, self).__init__()
+        # HyperParameters
+        self.embedding_dim = embedding_dim
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        # Parameters
+        self.embedding = nn.Embedding(num_embeddings=vocab_size,embedding_dim=embedding_dim)
+        self.cells = nn.ModuleList([nn.GRUCell(input_size=2*embedding_dim, hidden_size=hidden_size)] +
+                                   [nn.GRUCell(input_size=hidden_size, hidden_size=hidden_size) for i in range(num_layers-1)])
+        self.hidden_attentions = nn.ModuleList([SelfAttention(input_vector_size=hidden_size, hidden_size=attention_hidden_size) for i in range(num_layers)])
+
+        self.attn = SelfAttention(input_vector_size=hidden_size, hidden_size=attention_hidden_size, input_context_size=hidden_size)
+        self.dropout1 = nn.Dropout(.3)
+        self.fc1 = nn.Linear(hidden_size,32)
+        self.relu1 = nn.ReLU()
+
+        self.dropout2 = nn.Dropout(.3)
+        self.fc2 = nn.Linear(32,vocab_size)
+
+    def init_hidden(self):
+        self.h_s = [torch.zeros((1,self.hidden_size),dtype=torch.float64) for i in range(self.num_layers)]
+
+    def forward(self, decoder_input, encoder_outputs):
+        # decoder_input: correct token
+        embedded_decoder_input = self.embedding(decoder_input.type(torch.float64))
+
+        print(embedded_decoder_input.shape)
+        print(encoder_outputs.shape)
+
+        out_vecs = []
+        for token in embedded_decoder_input.split(1):
+            print('Decoder input:',decoder_input.shape,decoder_input.type())
+            print('Hidden shape:',self.h_s[0].shape,self.h_s[0].type())
+
+            attn = self.attn.combine(encoder_outputs.type(torch.float64),self.h_s[0].type(torch.float64))
+            input_vec = torch.cat([attn,token])
+            # get all outputs (go up)
+            new_h_s = []
+            for h, cell in zip(self.h_s, self.cells):
+                input_vec = cell(input_vec, h)
+                new_h_s.append(input_vec)
+
+            self.h_s = []
+            # compute new hidden states using attention (go right)
+            for i, att in enumerate(self.hidden_attentions):
+                self.h_s.append(att.combine(torch.stack(new_h_s[i:])))
+
+            do1 = self.dropout1(self.h_s[-1].squeeze())
+            fc1 = self.fc1(do1)
+            fc1 = self.relu1(fc1)
+
+            do2 = self.dropout2(fc1)
+            fc2 = self.fc2(do2)
+            out = F.softmax(fc2,0)
+
+            out_vecs.append(out)
+        return out_vecs
